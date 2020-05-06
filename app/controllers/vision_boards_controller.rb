b6dbafab8f1e163d7be7826a1f4654a75a59169b
LisaHJung <lisahjung@gmail.com>
2020-05-06 16:11:21 -0600

    newly seeded data with image url that takes you to the image

--- app/controllers/vision_boards_controller.rb
+++ app/controllers/vision_boards_controller.rb
@@ -1,33 +1,32 @@
 class VisionBoardsController < ApplicationController
     def index
         @vision_boards = VisionBoard.all
-        render json: @vision_boards, include: [:destination], [:vision_boards]
-     end
- end 
+        render json: @vision_boards, include: [:destination, :vision_boards]
+     end 
 
- def show
-     @vision_board = VisionBoard.find(params[:id])
-     render json: @vision_board, include: [:destination], [:vision_boards]
- end 
+    def show
+        @vision_board = VisionBoard.find(params[:id])
+        render json: @vision_board, include: [:destination, :vision_boards]
+    end 
 
- def create
-     @vision_board = VisionBoard.create(
-        user_id: params[:user_id],
-        destination_id: params[:destination_id],
-        comment: params[:comment]
-     )
-     render json: @vision_board
- end
+    def create
+        @vision_board = VisionBoard.create(
+            user_id: params[:user_id],
+            destination_id: params[:destination_id],
+            comment: params[:comment]
+        )
+        render json: @vision_board
+    end
 
- def update
-    @vision_board = VisionBoard.find(params[:id])
-    @vision_board.update(
-        user_id: params[:user_id],
-        destination_id: params[:destination_id],
-        comment: params[:comment]
-    )
-    render json: @vision_board
-end 
+    def update
+        @vision_board = VisionBoard.find(params[:id])
+        @vision_board.update(
+            user_id: params[:user_id],
+            destination_id: params[:destination_id],
+            comment: params[:comment]
+        )
+        render json: @vision_board
+    end 
 
 def destroy
     @vision_boards = VisionBoard.all
@@ -35,4 +34,5 @@ def destroy
     @vision_board.destroy
     render json: @vision_boards
 end 
+
 end
